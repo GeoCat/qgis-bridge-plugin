9ce2a962913c445f88ad150896342ac5ee86aec5
volaya <volayaf@gmail.com>
2019-07-01 20:41:09 +0200

    started new style exporters

---
+++ geocatbridge/publish/geostyler.py
@@ -0,0 +1,239 @@
+import json
+from qgis.core import *
+
+def saveLayerStyleAsGeostyler(layer, filename):
+    s = json.dumps(processLayer(layer), indent=4, sort_keys=True)
+    with open(filename, "w") as f:
+        f.write(s)
+
+def processLayer(layer):
+    if layer.type() == layer.VectorLayer:
+        rules = []
+        renderer = layer.renderer()
+        if not isinstance(renderer, QgsRuleBasedRenderer):
+            renderer = QgsRuleBasedRenderer.convertFromRenderer(renderer)
+        if renderer is None:
+            pass #show error
+        for rule in renderer.rootRule().children():
+            rules.append(processRule(rule))
+        return  {"name": layer.name(), "rules": rules}
+
+def processRule(rule):
+    symbolizers = createSymbolizers(rule.symbol().clone())
+    name = rule.label()
+    ruledef = {"name": name,
+            "symbolizers": symbolizers}
+    if not(rule.isElse()):
+        filt = processExpression(rule.filterExpression())
+        if filt is not None:
+            ruledef["filter"] = filt
+    if rule.dependsOnScale():
+        scale = processRuleScale(rule)
+        ruledef["scaleDenominator"] = scale
+    return ruledef
+
+def processRuleScale(rule):
+    return {"min": rule.minimumScale(),
+            "max": rule.maximumScale()}
+
+def processExpression(expstr):
+    try:
+        if expstr:
+            exp = QgsExpression(expstr)
+            return walkExpression(exp.rootNode())
+        else:
+            return None
+    except:
+        return None
+
+def symbolProperty(symbolLayer, name, propertyConstant=-1):
+    ddProps = symbolLayer.dataDefinedProperties()
+    if propertyConstant in ddProps.propertyKeys():
+        return processExpression(ddProps.property(propertyConstant).asExpression()) or ""
+    else:
+        return symbolLayer.properties()[name]
+
+def getHexColor(color):
+    try:
+        r,g,b,a = str(color).split(",")
+        return '#%02x%02x%02x' % (int(r), int(g), int(b))
+    except:
+        return color
+        
+def createSymbolizers(symbol):
+    opacity = symbol.opacity()
+    symbolizers = []    
+    for sl in symbol.symbolLayers():
+        props = sl.properties()
+        symbolizer = None
+        if isinstance(sl, QgsSimpleMarkerSymbolLayer):
+            symbolizer = _markerSymbolizer(sl, opacity)            
+        elif isinstance(sl, QgsSimpleLineSymbolLayer):
+            color = getHexColor(symbolProperty(sl, "line_color", QgsSymbolLayer.PropertyStrokeColor))
+            width = symbolProperty(sl, "line_width", QgsSymbolLayer.PropertyStrokeColor)
+            lineWidthUnits = props["line_width_unit"]
+            lineStyle = symbolProperty(sl, "line_style", QgsSymbolLayer.PropertyStrokeStyle)
+            cap = symbolProperty(sl, "capstyle", QgsSymbolLayer.PropertyCapStyle)
+            cap = "butt" if cap == "flat" else cap
+            join = symbolProperty(sl, "joinstyle", QgsSymbolLayer.PropertyJoinStyle)
+            offset = sl.offset()
+            symbolizer = {"kind": "Line",
+                    "color": color,
+                    "opacity": opacity,
+                    "width": shape,
+                    "dasharray": lineStyle, #TODO check this
+                    "perpendicularOffset": offset,
+                    "cap": cap,
+                    "join": join
+                    }
+            if lineStyle != "solid":
+                symbolizer["dasharray"] = [5, 2]
+        elif isinstance(sl, QgsSimpleFillSymbolLayer):
+            symbolizer = _fillSymbolizer(sl, opacity)
+        elif isinstance(sl, QgsSVGFillSymbolLayer):
+            path = os.path.basename(sl.svgPath())
+            size = symbolProperty(sl, "width", QgsSymbolLayer.PropertyWidth)
+            color = sl.svgFillColor().name()
+            x, y = sl.offset().x(), sl.offset().y()
+            svg = {"kind": "Icon",
+                    "color": color,
+                    "opacity": opacity,
+                    "image": path,
+                    "size": size,
+                    }
+            symbolizer = _fillSymbolizer(sl, opacity)
+            symbolizer["graphicFill"] = svg
+        elif isinstance(sl, QgsPointPatternFillSymbolLayer):
+            subSymbolizer = _markerSymbolizer(sl.subSymbol().symbolLayer(0), opacity)
+            symbolizer = _fillSymbolizer(sl, opacity)
+            symbolizer["graphicFill"] = subSymbolizer
+        elif isinstance(sl, QgsSvgMarkerSymbolLayer):
+            symbolizer = _svgMarkerSymbolizer(sl, opacity)
+        
+        if symbolizer is not None:
+            symbolizers.append(symbolizer)
+
+    return symbolizers
+
+def _markerSymbolizer(sl, opacity):
+    props = sl.properties()
+    size = symbolProperty(sl, "size", QgsSymbolLayer.PropertySize)
+    units = props["size_unit"] #TODO: Use this
+    color = getHexColor(symbolProperty(sl, "color"))
+    outlineColor = getHexColor(symbolProperty(sl, "outline_color"))
+    outlineWidth = float(symbolProperty(sl, "outline_width", QgsSymbolLayer.PropertyStrokeWidth))
+    outlineWidth = 1 if str(outlineWidth) == 0.0 else outlineWidth
+    outlineStyle = symbolProperty(sl, "outline_style", QgsSymbolLayer.PropertyStrokeStyle)
+    shape = props["name"]
+    rotation = symbolProperty(sl, "angle", QgsSymbolLayer.PropertyAngle)
+    x, y = sl.offset().x(), sl.offset().y()
+    return {"kind": "Mark",
+            "color": color,
+            "opacity": opacity,
+            "wellKnownName": shape,
+            "radius": size,
+            "strokeColor": outlineColor,
+            "strokeWidth": outlineWidth,
+            "offset": [x, y],
+            "rotate": rotation
+            } 
+
+def _svgMarkerSymbolizer(sl, opacity):
+    props = sl.properties()
+    size = symbolProperty(sl, "size", QgsSymbolLayer.PropertySize)
+    color = getHexColor(symbolProperty(sl, "color"))
+    rotation = symbolProperty(sl, "angle", QgsSymbolLayer.PropertyAngle)
+    x, y = sl.offset().x(), sl.offset().y()
+    return {"kind": "Icon",
+            "color": color,
+            "opacity": opacity,
+            "image": sl.path(),
+            "size": size,
+            "offset": [x, y],
+            "rotate": rotation
+            } 
+
+def _fillSymbolizer(sl, opacity)
+    if props["style"] == "no":
+        fillAlpha = 0                
+    else:
+        fillAlpha = alpha            
+    color =  getHexColor(symbolProperty(sl, "color", QgsSymbolLayer.PropertyFillColor))
+    outlineColor =  getHexColor(symbolProperty(sl, "outline_color", QgsSymbolLayer.PropertyStrokeColor))
+    outlineStyle = symbolProperty(sl, "outline_style", QgsSymbolLayer.PropertyStrokeStyle)
+    outlineWidth = symbolProperty(sl, "outline_width", QgsSymbolLayer.PropertyStrokeWidth)
+    borderWidthUnits = props["outline_width_unit"]
+    x, y = sl.offset().x(), sl.offset().y()
+    symbolizer = {"kind": "Fill",
+                  "opacity": fillAlpha,
+                  "outlineColor": outlineColor,
+                  "color": color,
+                  "outlineWidth": outlineWidth}
+    if outlineStyle not in ["solid", "no"]:
+        symbolizer["outlineDasharray"] = [5, 2]
+    if x or y:
+        symbolizer["translate"] = [x,y]
+    return symbolizer
+
+#######################33
+
+binary_ops = [
+    "||", "&&",
+    "==", "!=", "<=", ">=", "<", ">", "~",
+    "LIKE", "NOT LIKE", "ILIKE", "NOT ILIKE", "===", "!==",
+    "+", "-", "*", "/", "//", "%", "^",
+    "+"
+]
+
+unary_ops = ["!", "-"]
+
+def walkExpression(node):
+    try:
+        if node.nodeType() == QgsExpressionNode.ntBinaryOperator:
+            filt = handle_binary(node)
+        elif node.nodeType() == QgsExpressionNode.ntUnaryOperator:
+            filt = handle_unary(node)
+        #elif node.nodeType() == QgsExpressionNode.ntInOperator:
+            #filt = handle_in(node)
+        #elif node.nodeType() == QgsExpression.ntFunction:
+        #    filt = handle_function(node)
+        elif node.nodeType() == QgsExpressionNode.ntLiteral:
+            filt = handle_literal(node)
+        elif node.nodeType() == QgsExpressionNode.ntColumnRef:
+            filt = handle_columnRef(node)
+        #elif node.nodeType() == QgsExpression.ntCondition:
+        #    filt = handle_condition(nod)
+        return filt
+    except:
+        return []
+
+def handle_binary(node):
+    op = node.op()
+    retOp = binary_ops[op]
+    left = node.opLeft()
+    right = node.opRight()
+    retLeft = walkExpression(left)
+    retRight = walkExpression(right)
+    return [retOp, retLeft, retRight]
+
+def handle_unary(node):
+    op = node.op()
+    operand = node.operand()
+    retOp = unary_ops[op]
+    retOperand = walkExpression(operand)
+    return [retOp, retOperand]
+
+
+def handle_literal(node):
+    val = node.value()
+    quote = ""
+    if isinstance(val, basestring):
+        quote = "'"
+        val = val.replace("\n", "\\n")
+    elif val is None:
+        val = "null"
+
+    return "%s%s%s" % (quote, unicode(val), quote)
+
+def handle_columnRef(node):
+    return node.name()
---
+++ geocatbridge/publish/sldexporter.py
@@ -0,0 +1,339 @@
+
+from qgis.core import *
+from xml.etree.ElementTree import Element, SubElement
+from xml.etree import ElementTree
+from xml.dom import minidom
+
+def saveLayerStyleAsSld(layer, filename):
+    xml = processLayer(layer)    
+    #dom = minidom.parseString()
+    with open(filename, "w") as f:
+        f.write(ElementTree.tostring(xml, encoding='utf8', method='xml').decode())
+
+def processLayer(layer):
+    attribs = {
+        "version": "1.0.0",
+        "xsi:schemaLocation": "http://www.opengis.net/sld StyledLayerDescriptor.xsd",
+        "xmlns": "http://www.opengis.net/sld",
+        "xmlns:ogc": "http://www.opengis.net/ogc",
+        "xmlns:xlink": "http://www.w3.org/1999/xlink",
+        "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance"
+        }
+    root = Element("StyledLayerDescriptor", attrib=attribs) 
+    namedLayer = SubElement(root, "NamedLayer")
+    layerName = SubElement(namedLayer, "Name")
+    layerName.text = layer.name()
+    userStyle = SubElement(namedLayer, "UserStyle")
+    userStyleTitle = SubElement(userStyle, "Title")
+    userStyleTitle.text = layer.name()
+    if layer.type() == layer.VectorLayer:
+        rules = []
+        renderer = layer.renderer()
+        if not isinstance(renderer, QgsRuleBasedRenderer):
+            renderer = QgsRuleBasedRenderer.convertFromRenderer(renderer)
+        if renderer is None:
+            pass #show error          
+        featureTypeStyle = SubElement(userStyle, "FeatureTypeStyle")
+        for rule in renderer.rootRule().children():
+            featureTypeStyle.append(processRule(rule))
+    
+    return root
+
+def processRule(rule):    
+    ruleElement = Element("Rule")
+    ruleName = SubElement(ruleElement, "Name")
+    ruleName.text = rule.label()
+    symbolizers = createSymbolizers(rule.symbol().clone())
+    ruleElement.extend(symbolizers)
+    filt = processExpression(rule.filterExpression())    
+    if filt is not None:
+        filterElement = Element("ogc:Filter")
+        filterElement.append(filt)
+        ruleElement.append(filterElement)            
+    if rule.dependsOnScale():
+        maxScale = SubElement(ruleElement, "MaxScaleDenominator")
+        maxScale.text = rule.maximumScale()
+        minScale = SubElement(ruleElement, "MinScaleDenominator")
+        maxScale.text = rule.minimumScale()
+        
+    return ruleElement
+
+def processExpression(expstr):
+    try:
+        if expstr:
+            exp = QgsExpression(expstr)
+            return walkExpression(exp.rootNode())
+        else:
+            return None
+    except:
+        raise
+        return None
+
+def symbolProperty(symbolLayer, name, propertyConstant=-1):
+    ddProps = symbolLayer.dataDefinedProperties()
+    if propertyConstant in ddProps.propertyKeys():
+        return processExpression(ddProps.property(propertyConstant).asExpression()) or ""
+    else:
+        return symbolLayer.properties()[name]
+
+def getHexColor(color):
+    try:
+        r,g,b,a = str(color).split(",")
+        return '#%02x%02x%02x' % (int(r), int(g), int(b))
+    except:
+        return color
+        
+def createSymbolizers(symbol):
+    opacity = symbol.opacity()
+    symbolizers = []    
+    for sl in symbol.symbolLayers():
+        props = sl.properties()
+        symbolizer = None
+        if isinstance(sl, QgsSimpleMarkerSymbolLayer):
+            symbolizer = _simpleMarkerSymbolizer(sl, opacity)            
+        elif isinstance(sl, QgsSimpleLineSymbolLayer):
+            symbolizer = _lineSymbolizer(sl, opacity)            
+        elif isinstance(sl, QgsSimpleFillSymbolLayer):
+            symbolizer = _simpleFillSymbolizer(sl, opacity)
+        elif isinstance(sl, QgsPointPatternFillSymbolLayer):
+            symbolizer = _pointPatternFillSymbolizer(sl, opacity)
+        elif isinstance(sl, QgsSvgMarkerSymbolLayer):
+            symbolizer = _svgMarkerSymbolizer(sl, opacity)
+        
+        if symbolizer is not None:
+            symbolizers.append(symbolizer)
+
+    return symbolizers
+
+def _addCssParameter(parent, name, value):
+    sub = SubElement(parent, "CssParameter", name=name)
+    if isinstance(value, Element):
+        sub.append(value)
+    else:
+        sub.text = str(value)
+    return sub
+
+def _addSubElement(parent, tag, value=None):
+    sub = SubElement(parent, tag)
+    if value is not None:
+        if isinstance(value, Element):
+            sub.append(value)
+        else:
+            sub.text = str(value)
+    return sub
+
+def _lineSymbolizer(sl, opacity):
+    color = getHexColor(symbolProperty(sl, "line_color", QgsSymbolLayer.PropertyStrokeColor))
+    width = symbolProperty(sl, "line_width", QgsSymbolLayer.PropertyStrokeColor)
+    lineWidthUnits = props["line_width_unit"]
+    lineStyle = symbolProperty(sl, "line_style", QgsSymbolLayer.PropertyStrokeStyle)
+    cap = symbolProperty(sl, "capstyle", QgsSymbolLayer.PropertyCapStyle)
+    cap = "butt" if cap == "flat" else cap
+    join = symbolProperty(sl, "joinstyle", QgsSymbolLayer.PropertyJoinStyle)
+    offset = sl.offset()
+
+    root = Element("LineSymbolizer")
+    stroke = SubElement(root, "Stroke")
+    _addCssParameter(stroke, "stroke", color)
+    _addCssParameter(stroke, "stroke-width", width)
+    _addCssParameter(stroke, "stroke-opacity", opacity)
+    _addCssParameter(stroke, "stroke-linejoin", join)
+    _addCssParameter(stroke, "stroke-linecap", cap)
+    if lineStyle != "solid":        
+        _addCssParameter(stroke, "stroke-dasharray", "5 2")
+    if offset:
+        _addSubElement(root, "PerpendicularOffset", offset)
+    return root
+    
+def _svgMarkerSymbolizer(sl, opacity):
+    root, graphic = _basePointSimbolizer(sl, opacity)
+    svg = _svgGraphic(sl, opacity)
+    graphic.append(svg)
+    return root
+
+def _simpleMarkerSymbolizer(sl, opacity):
+    root, graphic = _basePointSimbolizer(sl, opacity)
+    mark = _markGraphic(sl, opacity)
+    graphic.append(mark)
+    return root
+
+def _basePointSimbolizer(sl, opacity):
+    props = sl.properties()
+    size = symbolProperty(sl, "size", QgsSymbolLayer.PropertySize)
+    units = props["size_unit"] #TODO: Use this
+    rotation = symbolProperty(sl, "angle", QgsSymbolLayer.PropertyAngle)
+    x, y = sl.offset().x(), sl.offset().y()
+    
+    root = Element("PointSymbolizer")
+    graphic = SubElement(root, "Graphic")
+    _addSubElement(graphic, "Size", size)
+    _addSubElement(graphic, "Rotation", rotation)
+    _addSubElement(graphic, "Opacity", opacity)
+    if x or y:
+        geom = SubElement(root, "Geometry")
+        func = SubElement(geom, "ogc:Function", name="offset")
+        _addSubElement(func, "ogc:PropertyName", geom)
+        _addSubElement(func, "ogc:Literal", x)
+        _addSubElement(func, "ogc:Literal", y)
+    return root, graphic
+
+def _markGraphic(sl, opacity):
+    props = sl.properties()
+    color = getHexColor(symbolProperty(sl, "color"))
+    outlineColor = getHexColor(symbolProperty(sl, "outline_color"))
+    outlineWidth = symbolProperty(sl, "outline_width", QgsSymbolLayer.PropertyStrokeWidth)
+    outlineWidth = 1 if str(outlineWidth) == 0.0 else outlineWidth
+    outlineStyle = symbolProperty(sl, "outline_style", QgsSymbolLayer.PropertyStrokeStyle)
+    shape = props["name"]
+    mark = Element("Mark")
+    _addSubElement(mark, "WellKnownName", shape)    
+    fill = SubElement(mark, "Fill")
+    _addCssParameter(fill, "fill", color)
+    _addCssParameter(fill, "fill-opacity", opacity)
+    stroke = SubElement(mark, "Stroke")
+    _addCssParameter(stroke, "stroke", outlineColor)
+    _addCssParameter(stroke, "stroke-width", outlineWidth)
+    _addCssParameter(stroke, "stroke-opacity", opacity)
+
+    return mark
+
+def _svgGraphic(sl):
+    path = sl.path()
+    externalGraphic = Element("ExternalGraphic")  
+    attrib = {
+        "xlink:type": "simple",
+        "xlink:href": sl.path()
+    } 
+    SubElement(externalGraphic, "OnlineResource", attrib=attrib)
+    _addSubElement(externalGraphic, "Format", "image/svg+xml") 
+    return externalGraphic   
+
+def _baseFillSymbolizer(sl, opacity):
+    root = Element("PolygonSymbolizer")        
+
+    return root
+
+def _pointPatternFillSymbolizer(sl, opacity):    
+    root = _baseFillSymbolizer(sl, opacity)
+    subsymbol = sl.subSymbol().symbolLayer(0)
+    size = symbolProperty(subsymbol, "size", QgsSymbolLayer.PropertySize)    
+    if isinstance(subsymbol, QgsSimpleMarkerSymbolLayer):
+        marker = _markGraphic(subsymbol, opacity)
+    elif isinstance(sl, QgsSvgMarkerSymbolLayer):
+        marker = _svgGraphic(subsymbol, opacity)
+    else:
+        marker = None # TODO
+
+    fill = _addSubElement(root, "fill")
+    graphicFill = _addSubElement(root, "GraphicFill")
+    graphic = _addSubElement(graphicFill, "Graphic")    
+    if marker is not None:
+        graphic.append(marker)
+    _addSubElement(graphicFill, "Size", size)
+    return root
+
+def _simpleFillSymbolizer(sl, opacity):
+    props = sl.properties()
+    if props["style"] == "no":
+        fillAlpha = 0                
+    else:
+        fillAlpha = opacity
+    color =  getHexColor(symbolProperty(sl, "color", QgsSymbolLayer.PropertyFillColor))        
+    root = _baseFillSymbolizer(sl, opacity)
+    fill = SubElement(root, "Fill")
+    _addCssParameter(fill, "fill", color)
+    _addCssParameter(fill, "fill-opacity", fillAlpha)
+
+    outlineColor =  getHexColor(symbolProperty(sl, "outline_color", QgsSymbolLayer.PropertyStrokeColor))
+    outlineStyle = symbolProperty(sl, "outline_style", QgsSymbolLayer.PropertyStrokeStyle)
+    if outlineStyle != "no": 
+        outlineWidth = symbolProperty(sl, "outline_width", QgsSymbolLayer.PropertyStrokeWidth)
+    else:
+        outlineWidth = 0
+    borderWidthUnits = props["outline_width_unit"]
+    stroke = SubElement(root, "Stroke")
+    _addCssParameter(stroke, "stroke", outlineColor)
+    _addCssParameter(stroke, "stroke-width", outlineWidth)
+    _addCssParameter(stroke, "stroke-opacity", opacity)
+    #_addCssParameter(stroke, "stroke-linejoin", join)
+    #_addCssParameter(stroke, "stroke-linecap", cap)
+    if outlineStyle not in ["solid", "no"]:
+        _addCssParameter(stroke, "stroke-dasharray", "5 2")
+    
+    x, y = sl.offset().x(), sl.offset().y()    
+    if x or y:
+        geom = SubElement(root, "Geometry")
+        func = SubElement(geom, "ogc:Function", name="offset")
+        _addSubElement(func, "ogc:PropertyName", geom)
+        _addSubElement(func, "ogc:Literal", x)
+        _addSubElement(func, "ogc:Literal", y)
+
+    return root
+
+#######################
+
+binaryOps = [
+     "Or",
+    "And",
+     "PropertyIsEqualTo",
+     "PropertyIsNotEqualTo",
+     "PropertyIsLessThanOrEqualTo",
+     "PropertyIsGreaterThanEqualTo",
+     "PropertyIsLessThan",
+     "PropertyIsGreater",
+     None, None, None, None, None, None, None,
+     "Add",
+      "Sub",
+      "Mul",
+      "Div",
+      None, None, None, None]
+
+unaryOps = ["Not", None]
+
+def walkExpression(node):
+    if node.nodeType() == QgsExpressionNode.ntBinaryOperator:
+        exp = handleBinary(node)
+    elif node.nodeType() == QgsExpressionNode.ntUnaryOperator:
+        exp = handleUnary(node)
+    #elif node.nodeType() == QgsExpressionNode.ntInOperator:
+        #filt = handle_in(node)
+    #elif node.nodeType() == QgsExpression.ntFunction:
+    #    filt = handle_function(node)
+    elif node.nodeType() == QgsExpressionNode.ntLiteral:
+        exp = handleLiteral(node)
+    elif node.nodeType() == QgsExpressionNode.ntColumnRef:
+        exp = handleColumnName(node)
+    #elif node.nodeType() == QgsExpression.ntCondition:
+    #    filt = handle_condition(nod)
+    return exp
+
+def handleBinary(node):
+    op = node.op()
+    retOp = binaryOps[op]
+    left = node.opLeft()
+    right = node.opRight()
+    retLeft = walkExpression(left)
+    retRight = walkExpression(right)
+    element = Element(retOp)
+    element.append(retLeft)
+    element.append(retRight)
+    return element
+
+def handleUnary(node):
+    op = node.op()
+    operand = node.operand()
+    retOp = unaryOps[op]
+    retOperand = walkExpression(operand)
+    element = Element(retOp)
+    element.append(retOperand)
+    return element
+
+def handleLiteral(node):
+    elem = Element("Literal")
+    elem.text = str(node.value())
+    return elem
+
+def handleColumnName(node):
+    elem = Element("PropertyName")
+    elem.text = node.name()
+    return elem
