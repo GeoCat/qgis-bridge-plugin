87eadaaee7594b679d69dcd0b566e639dbfde191
Paul <paul.vangenuchten@geocat.net>
2019-09-18 09:02:47 +0200

    added to transifex

---
+++ geocatbridge/libs/qgiscommons2/i18n/qgiscommons_nl.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="nl_NL" version="2.0">
+<context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="gui/settings.py" line="91"/>
+        <source>Search...</source>
+        <translation>Zoeken...</translation>
+    </message>
+    <message>
+        <location filename="gui/settings.py" line="118"/>
+        <source>Configuration options</source>
+        <translation>Configuratie</translation>
+    </message>
+    <message>
+        <location filename="gui/settings.py" line="119"/>
+        <source>Enter setting name to filter list</source>
+        <translation>Type een instelling om te filteren</translation>
+    </message>
+    <message>
+        <location filename="gui/settings.py" line="120"/>
+        <source>Setting</source>
+        <translation>Instelling</translation>
+    </message>
+    <message>
+        <location filename="gui/settings.py" line="121"/>
+        <source>Value</source>
+        <translation>Waarde</translation>
+    </message>
+</context>
+<context>
+    <name>qgiscommons</name>
+    <message>
+        <location filename="gui/__init__.py" line="23"/>
+        <source>Plugin help...</source>
+        <translation>Plugin help...</translation>
+    </message>
+    <message>
+        <location filename="gui/__init__.py" line="59"/>
+        <source>About...</source>
+        <translation>Over...</translation>
+    </message>
+    <message>
+        <location filename="gui/__init__.py" line="78"/>
+        <source>Plugin info</source>
+        <translation>Plugin informatie</translation>
+    </message>
+    <message>
+        <location filename="gui/settings.py" line="51"/>
+        <source>Plugin Settings...</source>
+        <translation>Plugin instellingen...</translation>
+    </message>
+</context>
+</TS>
