66452de2627757719a5695d115eafe39aa5db657
volaya <volayaf@gmail.com>
2019-10-23 07:35:21 +0200

    removed file header

--- geocatbridge/__init__.py
+++ geocatbridge/__init__.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 
-__author__ = 'Victor Olaya'
-__date__ = 'April 2019'
-__copyright__ = '(C) 2019 Victor Olaya'
-
-# This will get replaced with a git SHA1 when you do a git archive
-
-__revision__ = '$Format:%H$'
-
 import sys
 import os
 import site
