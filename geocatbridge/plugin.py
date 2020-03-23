42392166a150b22df42ef60daff9666394fc4ba5
volaya <volayaf@gmail.com>
2020-03-23 17:21:35 +0100

    minor fixes after last commit

--- geocatbridge/plugin.py
+++ geocatbridge/plugin.py
@@ -143,8 +143,7 @@ class GeocatBridge:
         if KEY_NAME in authManager.configIds():
             authConfig = QgsAuthMethodConfig()
             authManager.loadAuthenticationConfig(KEY_NAME, authConfig, True)            
-            key = authConfig.config('password2')
-            print(key)
+            key = authConfig.config('licensekey')
             if doEnterpriseLogin(key):
                 return True
         dlg = LoginDialog(self.iface.mainWindow())
--- geocatbridge/utils/enterprise.py
+++ geocatbridge/utils/enterprise.py
@@ -1,2 +1,2 @@
 def isEnterprise():
-	return True
+	return False
