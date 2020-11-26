637637f180c89ba47def294793075bb59ca7dfd7
Sander Schaminee <sander.schaminee@geocat.net>
2020-11-26 18:01:12 +0100

    Added function to retrieve icon paths

--- geocatbridge/utils/files.py
+++ geocatbridge/utils/files.py
@@ -1,26 +1,47 @@
 import os
 import shutil
 import uuid
+from pathlib import Path
 
 from qgis.PyQt.QtCore import QDir
 
+ICONS_DIR = Path(__file__).parent.parent / "icons"
+
+
 def tempFolder():
-    tempDir = os.path.join(QDir.tempPath(), "geocatbridge")
-    if not QDir(tempDir).exists():
-        QDir().mkpath(tempDir)
-    return os.path.abspath(tempDir)
+    temp_dir = os.path.join(QDir.tempPath(), "geocatbridge")
+    if not QDir(temp_dir).exists():
+        QDir().mkpath(temp_dir)
+    return os.path.abspath(temp_dir)
+
 
 def tempFolderInTempFolder():
     path = tempFolder()
-    folder = os.path.join(path, str(uuid.uuid4()).replace("-",""))
+    folder = os.path.join(path, str(uuid.uuid4()).replace("-", ""))
     if not QDir(folder).exists():
         QDir().mkpath(folder)
     return folder
+
 
 def tempFilenameInTempFolder(basename):
     folder = tempFolderInTempFolder()    
     filename = os.path.join(folder, basename)
     return filename
 
+
 def removeTempFolder():    
     shutil.rmtree(tempFolder())
+
+
+def getIconPath(name, ext=".png"):
+    """
+    Constructs the full icon path for a given base name.
+    If `name` does not have an extension, the given `ext` will be appended.
+    :param name:    The icon or image file name.
+    :param ext:     The default image extension (if not specified in the name).
+    :returns:       An icon file path string.
+    """
+    base_name = Path(name)
+    if not base_name.suffix:
+        base_name = base_name.with_suffix(ext)
+    return str(ICONS_DIR / base_name)
