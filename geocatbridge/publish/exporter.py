f9f35db0a5159411bdd17a97b81607469e375229
Sander Schaminee <sander.schaminee@geocat.net>
2020-12-03 10:49:52 +0100

    Use safe logging (if logger is missing/unusable)

--- geocatbridge/publish/exporter.py
+++ geocatbridge/publish/exporter.py
@@ -5,8 +5,12 @@ try:
 except (ModuleNotFoundError, ImportError):
     import gdal
 
-from qgis.core import QgsVectorFileWriter, QgsRasterFileWriter, QgsProject, Qgis
-from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (
+    QgsVectorFileWriter,
+    QgsRasterFileWriter,
+    QgsProject,
+    Qgis
+)
 
 from geocatbridge.utils.files import tempFilenameInTempFolder
 
@@ -19,13 +23,19 @@ def isSingleTableGpkg(layer):
     return ds.GetLayerCount() == 1
 
 
-def exportLayer(layer, fields=None, toShapefile=False, path=None, force=False, log=None):
+def exportLayer(layer, fields=None, to_shapefile=False, path=None, force=False, logger=None):
+
+    def safeLog(message):
+        if not logger or not hasattr(logger, 'logInfo'):
+            return
+        logger.logInfo(message)
+
     filename = layer.source().split("|")[0]
-    destFilename = layer.name()
+    dest_filename = layer.name()
     fields = fields or []
     if layer.type() == layer.VectorLayer:
-        if toShapefile and (force or layer.fields().count() != len(fields) or
-                            (os.path.splitext(filename.lower())[1] != EXT_SHAPEFILE)):
+        if to_shapefile and (force or layer.fields().count() != len(fields) or
+                             (os.path.splitext(filename.lower())[1] != EXT_SHAPEFILE)):
             # Export with Shapefile extension
             ext = EXT_SHAPEFILE
         elif force or os.path.splitext(filename.lower())[1] != EXT_GEOPACKAGE or \
@@ -34,46 +44,39 @@ def exportLayer(layer, fields=None, toShapefile=False, path=None, force=False, l
             ext = EXT_GEOPACKAGE
         else:
             # No need to export
-            if log is not None:
-                log.logInfo(QCoreApplication.translate("GeoCat Bridge",
-                                                       f"No need to export layer {destFilename} stored at {filename}"))
+            safeLog(f"No need to export layer {dest_filename} stored at {filename}")
             return filename
 
         # Perform GeoPackage or Shapefile export
         attrs = [i for i, f in enumerate(layer.fields()) if len(fields) == 0 or f.name() in fields]
-        output = path or tempFilenameInTempFolder(destFilename + ext)
+        output = path or tempFilenameInTempFolder(dest_filename + ext)
         if Qgis.QGIS_VERSION_INT < 31003:
             # Use writeAsVectorFormat for QGIS versions < 3.10.3 for backwards compatibility
-            QgsVectorFileWriter.writeAsVectorFormat(
+            # noinspection PyArgumentList
+            QgsVectorFileWriter().writeAsVectorFormat(
                 layer, output, fileEncoding="UTF-8", attributes=attrs,
                 driverName="ESRI Shapefile" if ext == EXT_SHAPEFILE else ""
             )
         else:
             # Use writeAsVectorFormatV2 for QGIS versions >= 3.10.3 to avoid DeprecationWarnings
-            transform_ctx = QgsProject.instance().transformContext()
+            transform_ctx = QgsProject().instance().transformContext()
             options = QgsVectorFileWriter.SaveVectorOptions()
             options.fileEncoding = "UTF-8"
             options.attributes = attrs
             options.driverName = "ESRI Shapefile" if ext == EXT_SHAPEFILE else ""
-            QgsVectorFileWriter.writeAsVectorFormatV2(layer, output, transform_ctx, options)
-        if log is not None:
-            log.logInfo(QCoreApplication.translate("GeoCat Bridge",
-                                                   f"Layer {destFilename} exported to {output}"))
+            QgsVectorFileWriter().writeAsVectorFormatV2(layer, output, transform_ctx, options)
+        safeLog(f"Layer {dest_filename} exported to {output}")
         return output
     else:
         # Export raster
         if force or not filename.lower().endswith("tif"):
-            output = path or tempFilenameInTempFolder(destFilename + ".tif")
+            output = path or tempFilenameInTempFolder(dest_filename + ".tif")
             writer = QgsRasterFileWriter(output)
             writer.setOutputFormat("GTiff")
             writer.writeRaster(layer.pipe(), layer.width(), layer.height(), layer.extent(), layer.crs())
             del writer
-            if log is not None:
-                log.logInfo(QCoreApplication.translate("GeoCat Bridge",
-                                                       f"Layer {destFilename} exported to {output}"))
+            safeLog(f"Layer {dest_filename} exported to {output}")
             return output
         else:
-            if log is not None:
-                log.logInfo(QCoreApplication.translate("GeoCat Bridge",
-                                                       f"No need to export layer {destFilename} stored at {filename}"))
+            safeLog(f"No need to export layer {dest_filename} stored at {filename}")
             return filename
