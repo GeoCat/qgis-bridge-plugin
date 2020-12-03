33dcbba2273b8a2a83a798fc2d70996b764ecd13
Sander Schaminee <sander.schaminee@geocat.net>
2020-12-03 10:23:44 +0100

    Added loadUiType helper function

--- geocatbridge/utils/gui.py
+++ geocatbridge/utils/gui.py
@@ -1,6 +1,17 @@
 from qgis.PyQt.QtWidgets import QApplication
 from qgis.PyQt.QtGui import QCursor
 from qgis.PyQt.QtCore import Qt, QCoreApplication
+from qgis.PyQt import uic
+
+from geocatbridge.utils import files
+
+
+def loadUiType(controller):
+    """ Returns a tuple of (widget, base) from a given controller (Python module path). """
+    ui_name = files.Path(controller).stem
+    ui_file = files.getUiPath(ui_name)
+    return uic.loadUiType(ui_file)
+
 
 def execute(func):
     QApplication.setOverrideCursor(QCursor(Qt.WaitCursor))
