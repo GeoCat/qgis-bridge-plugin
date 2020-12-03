fb32ca994f6db8756b8c23bd727c95600f22fdc6
Sander Schaminee <sander.schaminee@geocat.net>
2020-12-03 10:43:07 +0100

    Formatted and improved exception handling

--- geocatbridge/publish/ftpupload.py
+++ geocatbridge/publish/ftpupload.py
@@ -1,64 +1,57 @@
 import ftplib
+import os
 
-#Code adapted from the ftp exporter included in the qgis2web plugin, by Nyall Dawson
-def uploadFolder(folder, host, port, remoteFolder, username, password):
-        ftp = ftplib.FTP()
+
+# Code adopted from the ftp exporter included in the qgis2web plugin, by Nyall Dawson
+def uploadFolder(folder, host, port, remote_folder, username, password):
+    ftp = ftplib.FTP()
+    try:
+        ftp.connect(host, port)
+    except ftplib.all_errors as e:
+        raise Exception(f"Could not connect to FTP server at '{host}': {e}")
+
+    try:
+        ftp.login(username, password)
+    except ftplib.all_errors as e:
+        raise Exception(f"Login failed for user '{username}': {e}")
+
+    def cwdAndCreate(p):
+        """
+        Recursively changes directory to an FTP target,
+        creating new folders as required.
+        """
+        if not p:
+            return
         try:
-            ftp.connect(host, port)
-        except Exception:
-            raise Exception('Could not connect to ftp server.')            
+            ftp.cwd(p)
+        except ftplib.all_errors:
+            parent, base = os.path.split(p)
+            cwdAndCreate(parent)
+            if base:
+                ftp.mkd(base)
+                ftp.cwd(base)
 
-        #feedback.showFeedback('Connected!')
-        #feedback.showFeedback('Logging in as {}...'.format(self.username))
-        
-        try:
-            ftp.login(username, password)
-        except Exception:
-            raise Exception("Login failed for user {}".format(username))            
+    cwdAndCreate(remote_folder)
 
-        #feedback.showFeedback('Logged in to {}'.format(self.host))
+    def uploadPath(path):
+        files = os.listdir(path)
+        os.chdir(path)
+        for f in files:
+            current_path = os.path.join(path, f)
+            if os.path.isfile(current_path):
+                fh = open(f, 'rb')
+                ftp.storbinary('STOR %s' % f, fh)
+                fh.close()
+            elif os.path.isdir(current_path):
+                try:
+                    ftp.mkd(f)
+                except ftplib.all_errors:
+                    pass
+                ftp.cwd(f)
+                if not uploadPath(current_path):
+                    return False
+        ftp.cwd('..')
+        os.chdir('..')
 
-        def cwdAndCreate(p):
-            """
-            recursively changes directory to an ftp target,
-            creating new folders as required.
-            """
-            if not p:
-                return
-            try:
-                ftp.cwd(p)
-            except Exception:
-                parent, base = os.path.split(p)
-                cwdAndCreate(parent)
-                if base:                    
-                    ftp.mkd(base)
-                    ftp.cwd(base)
-
-        cwdAndCreate(remoteFolder)
-        
-        def uploadPath(path):        
-            files = os.listdir(path)
-            os.chdir(path)
-            for f in files:                
-                currentPath = os.path.join(path, f)
-                if os.path.isfile(currentPath):
-                    #feedback.showFeedback('Uploading {}'.format(f))
-                    fh = open(f, 'rb')
-                    ftp.storbinary('STOR %s' % f, fh)
-                    fh.close()                    
-                elif os.path.isdir(currentPath):
-                    #feedback.showFeedback('Creating folder {}'.format(f))
-                    try:
-                        ftp.mkd(f)
-                    except Exception:
-                        pass
-                    ftp.cwd(f)
-                    if not uploadPath(currentPath):
-                        return False
-            ftp.cwd('..')
-            os.chdir('..')            
-
-        uploadPath(folder)
-        
-        #feedback.setCompleted('Upload complete!')
-        ftp.close()        
+    uploadPath(folder)
+    ftp.close()
