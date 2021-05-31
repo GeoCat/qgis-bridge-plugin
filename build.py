1786a3691852d72fc7aa5a20eac488ed42150d50
Sander Schaminee <sander.schaminee@geocat.net>
2021-05-31 15:56:24 +0200

    Enable creating a zip package without docs

--- build.py
+++ build.py
@@ -4,36 +4,55 @@ import fnmatch
 import os
 import subprocess
 import zipfile
+import argparse
+from pathlib import Path
+
+OUTPUT_DIRNAME = 'build'
 
 
 def package():
-    builddocs()
-    package_file = "./geocatbridge.zip"
+    ap = argparse.ArgumentParser(description='Package GeoCat Bridge plugin')
+    ap.add_argument('--filename', help='ZIP file name', default='GeoCat_Bridge')
+    ap.add_argument('--docs', help='Add to include docs in the ZIP', default=False, action='store_true')
+    ns = ap.parse_args()
+
+    if ns.docs:
+        builddocs()
+    file_name = f'{ns.filename}.zip'
+    output_dir = Path(f'./{OUTPUT_DIRNAME}').resolve()
+    os.makedirs(output_dir, exist_ok=True)
+    package_file = output_dir / file_name
     with zipfile.ZipFile(package_file, "w", zipfile.ZIP_DEFLATED) as f:
-        make_zip(f)
+        make_zip(f, include_docs=ns.docs)
 
 
-def make_zip(zip_file, suffix=""):
-    print("Creating zip...")
-    excludes = {"test", "tests", '*.pyc', ".git"}
-    docs_dir = "./docs%s/build/latest" % suffix
+def make_zip(zip_file, suffix="", include_docs=True):
+    print("Creating ZIP for GeoCat Bridge plugin...")
+    file_excludes = {'*.pyc', "*.git", "*.log"}
+    dir_excludes = {"test", "tests", "_debug", "debug", "build", "__pycache__"}
     src_dir = "./geocatbridge%s" % suffix
 
     def filter_excludes(file_list):
         for fn in file_list:
-            if not any([fnmatch.fnmatch(fn, e) for e in excludes]):
+            if not any([fnmatch.fnmatch(fn, e) for e in file_excludes]):
                 yield fn
 
     for root, dirs, files in os.walk(src_dir):
+        if any(p in dir_excludes for p in Path(root).parts):
+            continue
         for f in filter_excludes(files):
             relpath = os.path.relpath(root, '.')
-            zip_file.write(os.path.join(root, f), os.path.join(relpath, f))
-        filter_excludes(dirs)
+            dstpath = os.path.join(relpath, f)
+            print(f'\t{dstpath}')
+            zip_file.write(os.path.join(root, f), dstpath)
 
-    for root, dirs, files in os.walk(docs_dir):
-        for f in files:
-            relpath = os.path.join("geocatbridge%s" % suffix, "docs", os.path.relpath(root, docs_dir))
-            zip_file.write(os.path.join(root, f), os.path.join(relpath, f))
+    if include_docs:
+        docs_dir = "./docs%s/build/latest" % suffix
+        for root, dirs, files in os.walk(docs_dir):
+            for f in files:
+                relpath = os.path.join("geocatbridge%s" % suffix, "docs", os.path.relpath(root, docs_dir))
+                zip_file.write(os.path.join(root, f), os.path.join(relpath, f))
+    print(f'Successfully created ZIP file {zip_file.filename}')
 
 
 def sh(commands):
